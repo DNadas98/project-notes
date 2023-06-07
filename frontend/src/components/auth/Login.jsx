eda524732e81a8377e3afe8bab7df3c9c7e45f4f
DNadas98 <125133206+DNadas98@users.noreply.github.com>
2023-06-07 17:38:42 +0200

    Add sample login form

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -1,14 +1,76 @@
-import React, { useContext } from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import AuthContext from "../../context/AuthProvider";
+
 function Login() {
-  const { setAuth } = useContext(AuthContext); //success: setAuth
+  const [successful, setSuccessful] = useState(false);
+  const [resultMessage, setResultMessage] = useState(null);
+  const [username, setUsername] = useState(null);
+  const [password, setPassword] = useState(null);
+
+  async function handleSubmit(event) {
+    try {
+      event.preventDefault();
+      if (!username || !password) {
+        setResultMessage("All fields are required");
+        return;
+      }
+      const valid = true;
+      if (valid) {
+        const url = "http://127.0.0.1:3001/auth/login";
+        const reqBody = JSON.stringify({ "username": username, "password": password });
+        const httpResponse = await fetch(url, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: reqBody
+        });
+        const jsonResponse = await httpResponse.json();
+        if (httpResponse.status === 200) {
+          setResultMessage("Login successful");
+          setSuccessful(true);
+        } else if (jsonResponse?.message) {
+          setResultMessage(jsonResponse.message);
+        }
+      }
+    } catch (err) {
+      setResultMessage("Error");
+    }
+  }
+
   return (
     <section className="auth_login">
       <h1>Login</h1>
-      <Link to="/api">
-        <button>API</button>
-      </Link>
+      {resultMessage ? <p>{resultMessage}</p> : <p>Please enter your name and password to log in</p>}
+      {successful ? (
+        <Link to="/api">
+          <button>Api</button>
+        </Link>
+      ) : (
+        <form
+          onSubmit={(event) => {
+            handleSubmit(event);
+          }}
+        >
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            onChange={(event) => {
+              setUsername(event.target.value);
+            }}
+          />
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            onChange={(event) => {
+              setPassword(event.target.value);
+            }}
+          />
+          <button>Login</button>
+        </form>
+      )}
       <Link to="/">
         <button>Back</button>
       </Link>
